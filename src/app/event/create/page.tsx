a860187f56b8af54564a72fb5a722bf92dfc6e9a
Shom770 <shayaanwadkar@gmail.com>
2024-10-12 14:51:23 -0400

    finished phone ui for create page

--- src/app/event/create/page.tsx
+++ src/app/event/create/page.tsx
@@ -10,7 +10,7 @@ import teams from "@/app/data/teams.json";
 import { State, fetchEvent } from "@/app/lib/actions";
 import { useFormState } from "react-dom";
 import { chunked } from "@/app/lib/utils";
-import { MEDIUM_SCREEN_SIZE, LARGE_SCREEN_SIZE, SMALL_SCREEN_SIZE } from "@/app/lib/constants";
+import { MEDIUM_SCREEN_SIZE, LARGE_SCREEN_SIZE } from "@/app/lib/constants";
 
 
 export default function Page() {
@@ -20,7 +20,7 @@ export default function Page() {
         if (
             event.key == 'Enter'
             && !Number.isNaN(parsedTeamNumber) 
-            // && teamsInEvent.indexOf(parsedTeamNumber) == -1
+            && teamsInEvent.indexOf(parsedTeamNumber) == -1
             && parsedTeamNumber.toString() in teams
         ) {
             setTeams([...teamsInEvent, parseInt(teamNumberEntered)]);
@@ -76,33 +76,33 @@ export default function Page() {
 
     return (
         <div className="flex items-center justify-center w-screen h-screen">
-            <div className="flex flex-col w-5/6 md:w-3/4 h-[87.5vh] gap-4">
+            <div className="flex flex-col w-5/6 md:w-3/4 h-[97vh] md:h-[87.5vh] gap-4">
                 <div className="inline-block flex-col items-start justify-center border-b border-[#929292]/50 w-auto basis-1/5">
-                    <h1 className={`${rethinkSans.className} antialiased text-[51px] md:text-6xl text-blue-600 font-extrabold`}>create an event</h1>
-                    <div className="w-full md:w-[25rem] h-16">
+                    <h1 className={`${rethinkSans.className} antialiased text-[12.3vw] md:text-6xl text-blue-600 font-extrabold`}>create an event</h1>
+                    <div className="w-full md:w-[25rem] h-20 md:h-16">
                         <p className="md:mt-3 mb-1">Search an event code</p>
                         <form action={formAction}>
-                            <div className="relative flex flex-row gap-2 w-full h-10">
+                            <div className="relative flex flex-row gap-2 w-full h-12 md:h-10">
                                 <div className="relative flex flex-1 shrink-0">
                                     <input 
                                         name="eventCode"
-                                        className="block rounded-md bg-transparent border border-blue-500/75 placeholder-gray-500 text-sm text-white w-full pl-10 focus:outline-none" 
+                                        className="block rounded-md bg-transparent border border-blue-500/75 placeholder-gray-500 text-base md:text-sm text-white w-full pl-10 focus:outline-none" 
                                         placeholder="Event code" />
                                     <MagnifyingGlassIcon className="absolute left-3 top-1/2 -translate-y-1/2 text-blue-500 w-5 h-5" />
                                 </div>
                                 <button
                                     key="createEvent"
                                     type="submit"
-                                    className="flex items-center justify-center w-10 h-10 rounded-md bg-blue-600 hover:bg-blue-500 text-[#0d111b]"
+                                    className="flex items-center justify-center w-12 md:w-10 h-12 md:h-10 rounded-md bg-blue-600 hover:bg-blue-500 text-[#0d111b]"
                                 >
-                                    <PaperAirplaneIcon className="w-5 h-5" />
+                                    <PaperAirplaneIcon className="w-6 md:w-5 h-6 md:h-5" />
                                 </button>
                             </div>
                         </form>
                     </div>
-                    <div className="py-2 mb-1 md:m-none">
+                    <div className="py-2 md:m-none">
                         { state.errors && 
-                            <p className="text-red-400 text-sm">{state.errors[0]}</p>
+                            <p className="text-red-400 text-xs md:text-sm">{state.errors[0]}</p>
                         }
                     </div>
                 </div>
@@ -110,11 +110,11 @@ export default function Page() {
                     <div className="inline-block w-full md:w-auto">
                         <p>Or, select teams to create an event for</p>
                         <p className="text-xs text-gray-400 -mt-1">Press enter to add a new team</p>
-                        <div className="flex flex-col gap-2 w-full h-24 mt-2">
-                            <div className="w-full h-10">
-                                <div className="relative flex flex-1 shrink-0 h-10">
+                        <div className="flex flex-col gap-2 w-full h-36 md:h-24 mt-2">
+                            <div className="w-full h-14 md:h-10">
+                                <div className="relative flex flex-1 shrink-0 h-12 md:h-10">
                                     <input
-                                        className="block rounded-md bg-transparent border border-blue-500/75 placeholder-gray-500 text-sm text-white w-full pl-8 focus:outline-none" 
+                                        className="block rounded-md bg-transparent border border-blue-500/75 placeholder-gray-500 text-base md:text-sm text-white w-full pl-8 focus:outline-none" 
                                         value={teamNumberEntered}
                                         placeholder="Team Number"
                                         onChange={(event) => setTeamNumber(event.target.value)}
@@ -126,16 +126,16 @@ export default function Page() {
                             <Link
                                 key="createEvent"
                                 href={`/event?teams=${teamsInEvent.join('_')}`}
-                                className="flex items-center justify-center gap-2 w-full h-10 rounded-lg bg-blue-600 hover:bg-blue-500 text-[#0d111b]"
+                                className="flex items-center justify-center gap-2 w-full h-12 md:h-10 rounded-lg text-lg md:text-base -mt-1 md:mt-0 bg-blue-600 hover:bg-blue-500 text-[#0d111b]"
                             >
                                 <strong>Create Event</strong>
-                                <PaperAirplaneIcon className="w-5 h-5" />
+                                <PaperAirplaneIcon className="w-6 md:w-5 h-6 md:h-5" />
                             </Link>
                         </div>
                     </div>
-                    <div className="flex items-center justify-center my-4 w-full h-[50vh] rounded-xl bg-slate-800">
+                    <div className="flex items-center justify-center -mt-3 md:my-4 w-full h-[50vh] rounded-xl bg-slate-800">
                         <div className="w-full h-full px-2 py-8">
-                            <div className="flex flex-col items-center justify-start w-full h-full overflow-y-auto gap-7">
+                            <div className="flex flex-col items-center justify-start w-full h-full overflow-y-auto gap-3 md:gap-7">
                                 {
                                     chunkedTeams.map((subList) => 
                                         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 w-full">
@@ -143,14 +143,14 @@ export default function Page() {
                                                 subList.map(([teamNumber, isInList]) => {
                                                         if (isInList) {
                                                             return (
-                                                                <button key={teamNumber} onClick={(_) => setTeams(teamsInEvent.filter((value) => value != parseInt(teamNumber)))} className="mx-auto w-full md:w-40 h-14">
+                                                                <button key={teamNumber} onClick={(_) => setTeams(teamsInEvent.filter((value) => value != parseInt(teamNumber)))} className="mx-auto w-[85%] md:w-40 h-14">
                                                                     <TeamPill key={teamNumber} teamNumber={parseInt(teamNumber)} teamName={teams[teamNumber as keyof typeof teams]} />
                                                                 </button>
                                                             );
                                                         }
                                                         else {
                                                             return (
-                                                                <div className="flex items-center justify-center mx-auto w-full md:w-40 h-14 bg-blue-600/[0.1] border-2 border-blue-600/50 border-dashed rounded-2xl">
+                                                                <div className="flex items-center justify-center mx-auto w-[85%] md:w-40 h-14 bg-blue-600/[0.1] border-2 border-blue-600/50 border-dashed rounded-md md:rounded-2xl">
                                                                     <h1 className={`${rethinkSans.className} font-extrabold text-xl text-white/75`}>{teamNumberEntered}</h1>
                                                                 </div> 
                                                             )
@@ -161,20 +161,6 @@ export default function Page() {
                                         </div>
                                     )
                                 }
-                                {/* {
-                                    teamsInEvent.map((teamNumber) => 
-                                        <button key={teamNumber} onClick={(_) => setTeams(teamsInEvent.filter((value) => value != teamNumber))}>
-                                            <TeamPill key={teamNumber} teamNumber={teamNumber} teamName={teams[teamNumber.toString() as keyof typeof teams]} />
-                                        </button>
-                                    )
-                                }
-                                {
-                                    teamNumberEntered && (
-                                        <div className="flex items-center justify-center w-full md:w-40 h-14 bg-blue-600/[0.1] border-2 border-blue-600/50 border-dashed rounded-2xl">
-                                            <h1 className={`${rethinkSans.className} font-extrabold text-xl text-white/75`}>{teamNumberEntered}</h1>
-                                        </div> 
-                                    )
-                                } */}
                             </div>
                         </div>
                     </div>
--- src/app/ui/event/team-pill.tsx
+++ src/app/ui/event/team-pill.tsx
@@ -1,18 +1,19 @@
 import { rethinkSans } from "@/app/ui/fonts";
+import clsx from 'clsx';
 
 export default function TeamPill({ teamNumber, teamName }: { teamNumber: number, teamName: string }) {
     const teamNameSplitByWords = teamName.split(/\s+/);
-    const widthOfDiv = (teamNameSplitByWords.length <= 2) ? "w-1/4" : "w-1/2";
+    const widthOfDiv = (teamNameSplitByWords.length <= 2) ? "md:w-1/4" : "md:w-1/2";
     const paddingOfTeamNumber = (
         teamNumber.toString().length == 1 
         || (teamName.length >= 15 && teamNameSplitByWords.length <= 2)
         || (teamNumber.toString().length == 3 && teamNameSplitByWords.length <= 2)
-    ) ? "mr-2" : "ml-2"
+    ) ? "md:mr-2" : "md:ml-2"
 
     return (
-        <div className="peer flex flex-col md:flex-row items-start md:items-center justify-center w-full h-full bg-blue-600 rounded-lg md:rounded-2xl transition hover:translate-y-1 hover:bg-red-400/75">
-            <h1 className={`${rethinkSans.className} font-extrabold text-xl text-white peer-hover:text-[#0d111b] ml-5 md:${paddingOfTeamNumber}`}>{teamNumber}</h1>
-            <div className={`flex items-center justify-center w-auto md:${widthOfDiv} md:h-1/2 md:ml-2`}>
+        <div className="peer flex flex-col md:flex-row items-start md:items-center justify-center w-full h-full bg-blue-600 rounded-md md:rounded-2xl transition hover:translate-y-1 hover:bg-red-400/75">
+            <h1 className={clsx(`${rethinkSans.className} font-extrabold text-xl text-white peer-hover:text-[#0d111b] ml-5 md:ml-0`, paddingOfTeamNumber)}>{teamNumber}</h1>
+            <div className={clsx(`flex items-center justify-center md:h-1/2 md:ml-2`, widthOfDiv)}>
                 <p className="text-xs md:text-[0.55rem] text-white peer-hover:text-[#0d111b] text-wrap text-left ml-5 -mt-1 md:m-0">{teamName}</p>
             </div>
         </div>
