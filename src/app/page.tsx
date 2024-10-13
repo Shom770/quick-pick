8ea1cbd74c7aedb205189c6fd2874da2442e133b
Shom770 <shayaanwadkar@gmail.com>
2024-10-13 15:12:58 -0400

    one more time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,19 @@ import { rethinkSans } from "@/app/ui/fonts";
 export default function Home() {
   return (
     <div className="relative flex flex-grow flex-col md:flex-row items-center justify-start md:justify-center w-screen h-screen gap-8 overflow-hidden">
-      <div className="basis-1/5 mt-8">
+      <div className="md:hidden mt-12">
+        <Image src="/hero-phone.png" width={322} height={500} quality={100} alt="Phone version of quickpick's picklist page and event page." style={
+          {
+            WebkitMaskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 0) 75%)',
+            maskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 0) 75%)',
+            WebkitMaskSize: '100% 100%',
+            maskSize: '100% 100%',
+            WebkitMaskRepeat: 'no-repeat',
+            maskRepeat: 'no-repeat'
+          }
+        } />
+      </div>
+      <div className="basis-1/5 -mt-32 md:mt-0">
         <h1 className={`${rethinkSans.className} text-6xl md:text-7xl font-extrabold text-blue-600 whitespace-nowrap`}>quick, pick!</h1>
         <div className="max-w-[19rem] md:max-w-[22.5rem]">
           <p className="text-white text-md leading-tight mt-3">
@@ -24,18 +36,6 @@ export default function Home() {
       </div>
       <div className="hidden md:flex items-center justify-center">
         <Image src="/hero-desktop.png" width={722} height={764} quality={100} alt="Desktop version of quickpick's picklist page and event page." />
-      </div>
-      <div className="relative h-[50vh] md:hidden overflow-hidden">
-        <Image src="/hero-phone.png" width={452} height={700} quality={100} alt="Phone version of quickpick's picklist page and event page." style={
-          {
-            WebkitMaskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 0) 75%)',
-            maskImage: 'linear-gradient(to bottom, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 0) 75%)',
-            WebkitMaskSize: '100% 100%',
-            maskSize: '100% 100%',
-            WebkitMaskRepeat: 'no-repeat',
-            maskRepeat: 'no-repeat'
-          }
-        } />
       </div>
     </div>
   );
