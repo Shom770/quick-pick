71534344a486c1678d6b931ea50e25dfa972e87d
Shom770 <shayaanwadkar@gmail.com>
2024-10-13 02:06:14 -0400

    squish a little bit more

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -47,7 +47,7 @@ function EventPage() {
     if (data.length == 0) {
         return (
             <div className="flex flex-col items-center justify-start md:justify-center w-screen h-screen">
-                <div className="flex flex-col items-center justify-center gap-4 md:flex-none md:grid md:grid-cols-5 w-4/5 md:w-5/6 h-2/5 md:h-1/5">
+                <div className="flex flex-col items-center justify-center gap-4 md:flex-none md:grid md:grid-cols-5 w-4/5 md:w-5/6 h-1/3 md:h-1/5">
                     <div className="flex flex-col items-start justify-center">
                         <h1 className={`${rethinkSans.className} text-6xl md:text-7xl text-blue-500 font-extrabold`}>picklist</h1>
                         <form className="w-full md:w-[18rem] mt-3">
@@ -78,7 +78,7 @@ function EventPage() {
 
     return (
         <div className="flex flex-col items-center justify-start md:justify-center w-screen h-screen">
-            <div className="flex flex-col items-center justify-center gap-4 md:flex-none md:grid md:grid-cols-5 w-4/5 md:w-5/6 h-2/5 md:h-1/5">
+            <div className="flex flex-col items-center justify-center gap-4 md:flex-none md:grid md:grid-cols-5 w-4/5 md:w-5/6 h-1/3 md:h-1/5">
                 <div className="flex flex-col items-start justify-center">
                     <h1 className={`${rethinkSans.className} text-6xl md:text-7xl text-blue-500 font-extrabold`}>picklist</h1>
                     <form className="w-full md:w-[18rem] mt-3">
