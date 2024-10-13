eefaf6f41e0f951a36073971556839647a0a225a
Shom770 <shayaanwadkar@gmail.com>
2024-10-13 02:56:26 -0400

    decrease size of 'picklist'

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -47,9 +47,9 @@ function EventPage() {
     if (data.length == 0) {
         return (
             <div className="flex flex-col items-center justify-start md:justify-center w-screen max-h-screen h-auto md:h-screen overflow-y-hidden">
-                <div className="flex flex-col items-center justify-center gap-4 md:flex-none md:grid md:grid-cols-5 w-4/5 md:w-5/6 h-2/5 md:h-1/5 mt-4 md:mt-0">
+                <div className="flex flex-col items-center justify-center gap-4 mdx:flex-none md:grid md:grid-cols-5 w-4/5 md:w-5/6 h-2/5 md:h-1/5 mt-4 md:mt-0">
                     <div className="flex flex-col items-start justify-center">
-                        <h1 className={`${rethinkSans.className} text-6xl md:text-7xl text-blue-500 font-extrabold`}>picklist</h1>
+                        <h1 className={`${rethinkSans.className} text-[52px] md:text-7xl text-blue-500 font-extrabold`}>picklist</h1>
                         <form className="w-full md:w-[18rem] mt-3">
                             <label htmlFor="sortOrder" className="font-medium text-sm">Choose metric to sort by</label>
                             <select 
