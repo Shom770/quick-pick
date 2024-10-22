5e22e5d02e093c892454d3751fbff7b702d72def
Shom770 <shayaanwadkar@gmail.com>
2024-10-22 17:29:42 -0400

    Add database functionality, will add more stuff later.

---
+++ src/app/lib/database.ts
@@ -0,0 +1,12 @@
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.MONGODB_URI;
+const options = {};
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+client = new MongoClient(uri!, options);
+clientPromise = client.connect();
+
+export default clientPromise;
