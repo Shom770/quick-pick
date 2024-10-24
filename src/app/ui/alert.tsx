b5097cfd597044fedc8af95fb095ff2a15b28bc9
Shom770 <shayaanwadkar@gmail.com>
2024-10-24 19:08:05 -0400

    Fix alerts

--- src/app/ui/alert.tsx
+++ src/app/ui/alert.tsx
@@ -13,8 +13,8 @@ export default function Alert({ color, message } : { color: "Error" | "Success",
     }
     
     const iconToUse = {
-        Error: <ExclamationCircleIcon className="text-red-400 flex-shrink-0 inline w-3 h-3 md:w-4 md:h-4 me-2" />,
-        Success: <ShieldCheckIcon className="text-green-400 flex-shrink-0 inline w-3 h-3 md:w-4 md:h-4 me-2" />
+        Error: <ExclamationCircleIcon className="text-red-400 flex-shrink-0 inline w-4 h-4 me-2" />,
+        Success: <ShieldCheckIcon className="text-green-400 flex-shrink-0 inline w-4 h-4 me-2" />
     }
 
     useEffect(() => {
@@ -31,7 +31,7 @@ export default function Alert({ color, message } : { color: "Error" | "Success",
 
     return (
         <div
-            className={`fixed top-12 left-1/2 transform -translate-x-1/2 z-50 flex items-center py-4 w-5/6 md:w-auto md:px-8 md:mb-4 text-sm border rounded-lg transition-opacity ease-in-out duration-500 ${
+            className={`fixed top-12 left-1/2 transform -translate-x-1/2 z-50 flex items-center py-4 w-[90vw] md:w-auto md:px-8 md:mb-4 text-sm border rounded-lg transition-opacity ease-in-out duration-500 ${
                 fadingOut ? 'opacity-0' : 'opacity-100'
             } ${colorClasses[color]}`}
             role="alert"
